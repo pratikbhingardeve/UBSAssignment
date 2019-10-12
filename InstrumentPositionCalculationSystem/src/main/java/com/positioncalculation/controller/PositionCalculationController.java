581858126d07ff7c86b31905531f7c8859f5ab64
Blackpearl <Blackpearl@192.168.1.206>
2019-10-12 23:36:53 +0530

    Log updated with proper details

--- InstrumentPositionCalculationSystem/src/main/java/com/positioncalculation/controller/PositionCalculationController.java
+++ InstrumentPositionCalculationSystem/src/main/java/com/positioncalculation/controller/PositionCalculationController.java
@@ -59,10 +59,10 @@ public class PositionCalculationController {
 			 * Invoke service layer interface to calculate EOD positions and generate output feed.
 			 */
 			if(service.calculateEodPositionAndPrepareOutputFeed(outputEndOfDayPositionFileName)) {
-				logger.info("EOD Instrument Account position's Output Feed file successfully generated:{}",inputTransactionsFileName);
+				logger.info("EOD Instrument Account position's Output Feed file successfully generated:{}",outputEndOfDayPositionFileName);
 			}
 			else {
-				logger.info("Failed to calculate EOD position and generate output feed file:{}",inputTransactionsFileName);
+				logger.info("Failed to calculate EOD position and generate output feed file:{}",outputEndOfDayPositionFileName);
 				return false;
 			}
 			
